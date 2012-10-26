deb887c33af9c7742c6a03aedded3193553c01e7
h9379203 <h9379203@bee2b224-8ba5-bd40-ad2e-ed6148e7e4de>
2012-10-26 06:43:43 +0000

    mp2_decode library에 mp2_decode(), parseMp2Header(), getBitrate(), getBufSize() 추가

--- mp2_decode/mp2_decode.cpp
+++ mp2_decode/mp2_decode.cpp
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+static const int bitrateTable[2][15]={
+	{0,8,16,24,32,40,48,56,64,80,96,112,128,144,160},		//for 24KHz
+	{0,32,48,56,64,80,96,112,128,160,192,224,256,320,384}	//for 48KHz
+};
+
 int mp2Decode_Init()
 {
 	if(mpg123_init()!=MPG123_OK) { cout<<"mpg123_init() failed"; mp2Decode_Cleanup(); return -1; }
@@ -31,4 +36,51 @@ void mp2Decode_Cleanup(mpg123_handle* m)
 {
 	if(m) mpg123_delete(m);
 	mpg123_exit();
+}
+
+void parseMp2Header(MP2_HEADER* pHeader,unsigned char* pBuffer)
+{
+	pHeader->sync		= (((unsigned short)pBuffer[0]&0xFF)<<4) | (((unsigned short)pBuffer[1]&0xF0)>>4);
+	pHeader->id			= (pBuffer[1]&0x08)>>3;
+	pHeader->layer		= (pBuffer[1]&0x06)>>1;
+	pHeader->protect	= (pBuffer[1]&0x01);
+	pHeader->bitrateIdx	= (pBuffer[2]&0xF0)>>4;
+	pHeader->sampling	= (pBuffer[2]&0x0C)>>2;
+	pHeader->padding	= (pBuffer[2]&0x02)>>1;
+	pHeader->priv		= (pBuffer[2]&0x01);
+	pHeader->mode		= (pBuffer[3]&0xC0)>>6;
+	pHeader->mode_ext	= (pBuffer[3]&0x30)>>4;
+	pHeader->copyright	= (pBuffer[3]&0x08)>>3;
+	pHeader->orignal	= (pBuffer[3]&0x04)>>2;
+	pHeader->emphasis	= (pBuffer[3]&0x03);
+}
+
+int getBitrate(int nId,int nBitrateIdx)
+{
+	assert(nId==0 || nId==1);
+	assert(nBitrateIdx>0 && nBitrateIdx<15);
+	return 1000*bitrateTable[nId][nBitrateIdx];	//kbps unit
+}
+
+int getBufSize(int nBitrate, int nId)
+{
+	assert(nId==0 || nId==1);
+	assert(nBitrate>1000);
+
+	int nSrcBufferSize = 144*nBitrate/((nId)?48000:24000);
+
+	return nSrcBufferSize;
+}
+
+int mp2_decode(mpg123_handle *m, const unsigned char *mp2buffer, size_t nSrcBufferSize, unsigned char *pcmBuffer, size_t outmemsize, size_t *size)
+{
+	int ret = mpg123_decode(m,mp2buffer,nSrcBufferSize,pcmBuffer,outmemsize,size);
+	if(ret == MPG123_NEW_FORMAT)
+	{
+		long rate;
+		int channels, enc;
+		mpg123_getformat(m, &rate, &channels, &enc);
+		fprintf(stderr, "New format: %li Hz, %i channels, encoding value %i\n", rate, channels, enc);
+	}
+	return ret;
 }
--- mp2_decode/mp2_decode.h
+++ mp2_decode/mp2_decode.h
@@ -19,10 +19,32 @@ extern "C" {
 #define DLL_EXPORT __declspec(dllexport)
 #endif
 
+#pragma pack(push, 1)
+	typedef struct{
+		unsigned short sync:12;
+		unsigned short id:1;
+		unsigned short layer:2;
+		unsigned short protect:1;
+		unsigned short bitrateIdx:4;
+		unsigned short sampling:2;
+		unsigned short padding:1;
+		unsigned short priv:1;
+		unsigned short mode:2;
+		unsigned short mode_ext:2;
+		unsigned short copyright:1;
+		unsigned short orignal:1;
+		unsigned short emphasis:2;
+	}MP2_HEADER;
+#pragma pack(pop)
+
 #pragma once
 
 DLL_EXPORT int mp2Decode_Init();
 DLL_EXPORT void mp2Decode_Cleanup(mpg123_handle* m=NULL);
+DLL_EXPORT void parseMp2Header(MP2_HEADER* pHeader,unsigned char* pBuffer);
+DLL_EXPORT int getBitrate(int nId,int nBitrateIdx);
+DLL_EXPORT int getBufSize(int nBitrate, int nId);
+DLL_EXPORT int mp2_decode(mpg123_handle *m, const unsigned char *mp2buffer, size_t nSrcBufferSize, unsigned char *pcmBuffer, size_t outmemsize, size_t *size);
 
 #ifdef __cplusplus
 }
