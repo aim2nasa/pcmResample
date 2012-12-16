7b4dc9331d269c1edab613be0a0fc3e522f43f67
skwak <skwak@bee2b224-8ba5-bd40-ad2e-ed6148e7e4de>
2012-12-16 17:12:15 +0000

    CResample클래스가 스테레오,모노 변환도 가능하도록 다시 클래스 기능을 구현함 (기존 버전은 스테레오->모노 변환이 안되도록 구현되어있었음) 스테레오->모노 변환 테스트 케이스 새로 작성 추가

--- ffmpeg_resample/test.cpp
+++ ffmpeg_resample/test.cpp
@@ -1,8 +1,17 @@
 #include <gtest/gtest.h>
 #include "CResample.h"
 
+#pragma once
+#ifndef INT64_C
+#define INT64_C(c) (c ## LL)
+#define UINT64_C(c) (c ## ULL)
+#endif
+
+#include <libavutil/samplefmt.h>
+
 #define REF_PCM_FRAME_SIZE		(1152*2)	//48khz, short형(2바이트) 기준
-#define SAMPLES_PER_SAMPLE_FREQ	48
+#define MONO_SAMPLES_PER_SAMPLE_FREQ	24
+#define STEREO_SAMPLES_PER_SAMPLE_FREQ	48
 
 //===================================================================
 //Table.1 SampleFreq와 sample size의 관계
@@ -16,33 +25,57 @@
 //1KHz당 샘플수 = 48개
 //===================================================================
 
-void resampTest(int nOutSampFreq,int nInpSampFreq,const char* pInpFIle,const char* pOutFile);
+void resampTest(int output_channels,int input_channels,int nOutSampFreq,int nInpSampFreq,const char* pInpFIle,const char* pOutFile);
 
-TEST(pcm_resample_test, From48khz_To_48khz)
+//Resample from Stereo to Stereo
+TEST(pcm_resample_test, From48khz_Stereo_To_16khz_Stereo)
 {
-	resampTest(48000,48000,"dump_48khz.pcm","resampled_48KHz.pcm");
+	resampTest(2,2,16000,48000,"dump_48khz_Stereo.pcm","resampled_16KHz_stereo.pcm");
 }
 
-TEST(pcm_resample_test, From48khz_To_16khz)
+TEST(pcm_resample_test, From48khz_Stereo_To_24khz_Stereo)
 {
-	resampTest(16000,48000,"dump_48khz.pcm","resampled_16KHz.pcm");
+	resampTest(2,2,24000,48000,"dump_48khz_Stereo.pcm","resampled_24KHz_stereo.pcm");
 }
 
-TEST(pcm_resample_test, From48khz_To_32khz)
+TEST(pcm_resample_test, From48khz_Stereo_To_32khz_Stereo)
 {
-	resampTest(32000,48000,"dump_48khz.pcm","resampled_32KHz.pcm");
+	resampTest(2,2,32000,48000,"dump_48khz_Stereo.pcm","resampled_32KHz_stereo.pcm");
 }
 
-TEST(pcm_resample_test, From48khz_To_24khz)
+TEST(pcm_resample_test, From48khz_Stereo_To_48khz_Stereo)
 {
-	resampTest(24000,48000,"dump_48khz.pcm","resampled_24KHz.pcm");
+	resampTest(2,2,48000,48000,"dump_48khz_Stereo.pcm","resampled_48KHz_stereo.pcm");
 }
 
-void resampTest(int nOutSampFreq,int nInpSampFreq,const char* pInpFIle,const char* pOutFile)
+//Resample from Stereo to Mono
+TEST(pcm_resample_test, From48khz_Stereo_To_16khz_Mono)
 {
+	resampTest(1,2,16000,48000,"dump_48khz_Stereo.pcm","resampled_16KHz_mono.pcm");
+}
+
+TEST(pcm_resample_test, From48khz_Stereo_To_24khz_Mono)
+{
+	resampTest(1,2,24000,48000,"dump_48khz_Stereo.pcm","resampled_24KHz_mono.pcm");
+}
+
+TEST(pcm_resample_test, From48khz_Stereo_To_32khz_Mono)
+{
+	resampTest(1,2,32000,48000,"dump_48khz_Stereo.pcm","resampled_32KHz_mono.pcm");
+}
+
+TEST(pcm_resample_test, From48khz_Stereo_To_48khz_Mono)
+{
+	resampTest(1,2,48000,48000,"dump_48khz_Stereo.pcm","resampled_48KHz_mono.pcm");
+}
+
+void resampTest(int output_channels,int input_channels,int nOutSampFreq,int nInpSampFreq,const char* pInpFIle,const char* pOutFile)
+{
+	assert(output_channels==1||output_channels==2);
+
 	CResample *pRsp = new CResample();
 
-	int nRtn = pRsp->init(nOutSampFreq,nInpSampFreq,16,10,1,0.0);
+	int nRtn = pRsp->init(output_channels,input_channels,nOutSampFreq,nInpSampFreq,AV_SAMPLE_FMT_S16,AV_SAMPLE_FMT_S16,16,10,1,0.0);
 	EXPECT_EQ(nRtn,RSP_OK);
 
 	FILE* inpFile,*outFile;
@@ -54,9 +87,10 @@ void resampTest(int nOutSampFreq,int nInpSampFreq,const char* pInpFIle,const cha
 	err = fopen_s(&outFile,pOutFile,"wb");
 	EXPECT_EQ(err, 0);
 
-	int nSamples = SAMPLES_PER_SAMPLE_FREQ*nOutSampFreq/1000;
+	int nSamples = 0;
+	if(output_channels==1) nSamples = MONO_SAMPLES_PER_SAMPLE_FREQ*nInpSampFreq/1000;
+	if(output_channels==2) nSamples = STEREO_SAMPLES_PER_SAMPLE_FREQ*nInpSampFreq/1000;
 
-	int nConsumed = 0;
 	size_t nSize = 0;
 	short inpBuffer[REF_PCM_FRAME_SIZE];
 	short outBuffer[REF_PCM_FRAME_SIZE];
@@ -65,10 +99,8 @@ void resampTest(int nOutSampFreq,int nInpSampFreq,const char* pInpFIle,const cha
 		if(nSize!=REF_PCM_FRAME_SIZE) 
 			break;
 
-		nRtn = pRsp->resample(outBuffer,inpBuffer,&nConsumed,nSize,REF_PCM_FRAME_SIZE);
+		nRtn = pRsp->resample(outBuffer,inpBuffer,nSamples);
 		EXPECT_GT(nRtn,0);
-		EXPECT_EQ(nSize,nConsumed);
-		EXPECT_EQ(nRtn,nSamples);	//쓰여진 샘플수가 샘플주파수에 의해 계산된 샘플 계산과 같아야 함 (Table.1)
 		nSize = fwrite(outBuffer,sizeof(short),nRtn,outFile);
 		EXPECT_EQ(nSize,nRtn);
 	}
